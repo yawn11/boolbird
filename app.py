3b030fe4c8567068624285da773d91d1d0989a1a
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-07 19:29:15 +0900

    real com1

--- app.py
+++ app.py
@@ -4,17 +4,11 @@ def main() :
     st.title("건설 안전사고 위험도 예측 서비스")
     st.text("텍스트를 입력하세요")
     st.text("텍스트를 입력하세요")
-    # title 쓰기
-    st.title('제목 쓰세요')
     # 그냥 text 쓰기 
     st.write('아무거나 쓰세요')
-    # markdown tag 쓰고 싶으면
-    st.markdown('<h1>태그를 쓸 수 있어요</h1>')
     # user input 받기 
     st.text_input('사용자 입력을 받아보세요: ')
 
-    # 이외에도 다양한 기능 엄청 많다~ 
-    st.button 
     st.sidebar 
     
     
