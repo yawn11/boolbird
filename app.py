5a5f4dfb9756ed4084d9be7af4e2ff8f87375e13
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-08 00:15:24 +0900

    a

--- app.py
+++ app.py
@@ -6,24 +6,24 @@ st.title("건설 안전사고 위험도 예측 서비스")
 st.write('아래 정보를 입력해주세요')
 st.title(" ")
 
-def preprocess_data():
+#def preprocess_data():
     # CSV 파일 경로
-    csv_file_path = 'output/output-v2.csv'
+    #csv_file_path = 'output/output-v2.csv'
 
     # CSV 파일 로드
-    df = pd.read_csv(csv_file_path)
+    #df = pd.read_csv(csv_file_path)
 
     # 시설물 종류 추출
-    facility_types = df['시설물 종류'].unique().tolist()
+    #facility_types = df['시설물 종류'].unique().tolist()
 
     # 공종 추출
-    job_types = df['공종'].unique().tolist()
+    #job_types = df['공종'].unique().tolist()
 
-    return facility_types, job_types
+    #return facility_types, job_types
 
 
 # 데이터 전처리 함수 호출하여 시설물 종류와 공종 추출
-facility_types, job_types = preprocess_data()
+#facility_types, job_types = preprocess_data()
 
 
 #공사기간 (yy.mm.dd ~ yy.mm.dd)
