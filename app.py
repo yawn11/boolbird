2fadb1ec088726ebf460d8e06c0218361eb32232
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-08 18:43:27 +0900

    sd

--- app.py
+++ app.py
@@ -18,8 +18,14 @@ with col2 :
 
 #------------아래는 입력 (총 10개)---------
 
-list = ["시설물 종류_건축","시설물 종류_산업환경설비","시설물 종류_조경","시설물 종류_토목","공공/민간 구분_공공","공공/민간 구분_민간","날씨_강설","날씨_강우","날씨_강풍","날씨_맑음","날씨_안개","날씨_흐림","공종_가설공사","공종_강구조물공사","공종_건축 토공사","공종_건축물 부대공사","공종_관공사","공종_관공사 부대공사","공종_교량공사","공종_금속공사","공종_기계설비공사","공종_기타","공종_댐 및 제방공사","공종_도로 및 포장공사","공종_도장공사","공종_말뚝공사","공종_목공사","공종_미장공사","공종_방수공사","공종_산업설비공사","공종_수장공사","공종_전기설비공사","공종_조경공사","공종_조적공사","공종_지반개량공사","공종_지반조사","공종_지붕 및 홈통공사","공종_지정공사","공종_창호 및 유리공사","공종_철골공사","공종_철근콘크리트공사","공종_철도 및 궤도공사","공종_타일 및 돌공사","공종_터널공사","공종_토공사","공종_통신설비공사","공종_특수 건축물공사","공종_프리캐스트 콘크리트공사","공종_하천공사","공종_항만공사","공종_해체 및 철거공사","발생일시","공사비","공사기간","공정률","작업자수","설계안전성검토","기온","습도"]
-df = pd.DataFrame()
+list = ["시설물 종류_건축","시설물 종류_산업환경설비","시설물 종류_조경","시설물 종류_토목",
+        "공공/민간 구분_공공","공공/민간 구분_민간",
+        "날씨_강설","날씨_강우","날씨_강풍","날씨_맑음","날씨_안개","날씨_흐림",
+        "공종_가설공사","공종_강구조물공사","공종_건축 토공사","공종_건축물 부대공사","공종_관공사","공종_관공사 부대공사","공종_교량공사","공종_금속공사","공종_기계설비공사","공종_기타","공종_댐 및 제방공사","공종_도로 및 포장공사","공종_도장공사","공종_말뚝공사","공종_목공사","공종_미장공사","공종_방수공사","공종_산업설비공사","공종_수장공사","공종_전기설비공사","공종_조경공사","공종_조적공사","공종_지반개량공사","공종_지반조사","공종_지붕 및 홈통공사","공종_지정공사","공종_창호 및 유리공사","공종_철골공사","공종_철근콘크리트공사","공종_철도 및 궤도공사","공종_타일 및 돌공사","공종_터널공사","공종_토공사","공종_통신설비공사","공종_특수 건축물공사","공종_프리캐스트 콘크리트공사","공종_하천공사","공종_항만공사","공종_해체 및 철거공사",
+        "발생일시","공사비","공사기간","공정률","작업자수","설계안전성검토","기온","습도"]
+
+df = pd.DataFrame({"class": list, "boolean": 0.0})
+df = df.set_index("class")
 
 #(1) 공사기간 (yy.mm.dd ~ yy.mm.dd)
 col1,empty2,col2 = st.columns([1, 0.03, 1])
@@ -33,26 +39,29 @@ start_day = parser.parse(start_day)
 end_day = parser.parse(end_day)
 dur = (end_day - start_day).days
 if (dur <= 0): st.write("공사기간 입력 오류")
-dur_row = ['공사기간', float(dur)]
+df.loc['공사기간'] = dur
 
 #(3) 시설물종류 (건축~)
 col1,empty2,col2 = st.columns([1, 0.03, 1])
 with col1 :
     facility = ['건축', '산업환경설비', '조경', '토목', '기타']
     selected_facility = st.selectbox('시설물종류를 선택해주세요',facility)
+string = '시설물 종류_'
+selected_facility = '건축'
+df.loc[string + selected_facility] = 1.0
 if (selected_facility == '건축'): 
-    facility_row = ['시설물 종류_건축', 1.0]
+    df.loc['시설물 종류_건축'] = 1.0
 elif (selected_facility == '산업환경설비'):
-    facility_row = ['시설물 종류_산업환경설비', 1.0]
+    df.loc['시설물 종류_산업환경설비'] = 1.0
 elif (selected_facility == '조경'):
-    facility_row = ['시설물 종류_조경', 1.0]
-else:
-    facility_row = ['시설물 종류_토목', 1.0]
+    df.loc['시설물 종류_조경'] = 1.0
+else: 
+    df.loc['시설물 종류_토목'] = 1.0
     
 #(4) 공정률 (~%)
 with col2 :
     percent = st.text_input('공정률을 입력해주세요  (단위 : %)', value="", placeholder="65%라면 '65'라고 입력해주세요")
-percent_row = ['공정률', float(percent)]
+df.loc['공정률'] = float(percent)
 
 #(6),(5) 공종 (철근콘크리트~)
 with col2:
@@ -90,30 +99,29 @@ with col2 :
 st.write(' ')
 button_clicked = st.button('위험도 예측 결과 확인')
 danger = ["하", "중", "상"]
-if button_clicked:
-    if True:
-        st.write(f"{danger[0]}")
+#if button_clicked:
+    #if 조건식1:
+        #st.write(f"위험도는",{danger[0]},\"입니다.")
     #elif 조건식2:
-        #st.write(f"{danger[1]}")
+        #st.write(f"위험도는",{danger[1]},\"입니다.")
     #elif 조건식3:
-        #st.write(f"{danger[2]}")
+        #st.write(f"위험도는",{danger[2]},\"입니다.")
     
     
-    
-    # Add a placeholder 진행 상황 바
-    # 전체 바의 스타일과 크기를 설정하는 CSS 스타일
-    bar_style = 'background-color: #F0F2F6; height: 8px; width: 100%;'
+# Add a placeholder 진행 상황 바
+# 전체 바의 스타일과 크기를 설정하는 CSS 스타일
+bar_style = 'background-color: #F0F2F6; height: 8px; width: 100%;'
 
-    # 빨간색 부분의 스타일과 크기를 설정하는 CSS 스타일
-    redred = 0.7  # 빨간색 부분의 비율 (0.0 ~ 1.0 사이의 값)
-    red_width = int(redred * 100)  # 빨간색 부분의 너비 계산
-    red_bar_style = f'background-color: #FF0000; height: 100%; width: {red_width}%;'
+# 빨간색 부분의 스타일과 크기를 설정하는 CSS 스타일
+redred = 0.7  # 빨간색 부분의 비율 (0.0 ~ 1.0 사이의 값)
+red_width = int(redred * 100)  # 빨간색 부분의 너비 계산
+red_bar_style = f'background-color: #FF0000; height: 100%; width: {red_width}%;'
 
-    # 바의 HTML 코드
-    bar = f'<div style="{bar_style}"><div style="{red_bar_style}"></div></div>'
+# 바의 HTML 코드
+bar = f'<div style="{bar_style}"><div style="{red_bar_style}"></div></div>'
 
-    # Streamlit에 표시
-    st.markdown(bar, unsafe_allow_html=True)
+# Streamlit에 표시
+st.markdown(bar, unsafe_allow_html=True)
 
 
 
