0fdbd558446e27a2bf5dbd3012f7a1c3fd9aa3dd
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-08 20:40:55 +0900

    sd

--- app.py
+++ app.py
@@ -189,30 +189,23 @@ button_clicked = st.button('위험도 예측 결과 확인') #통계페이지 �
 danger = ["하", "중", "상"]
 if button_clicked:
     bar_style = 'background-color: #F0F2F6; height: 8px; width: 100%;'
+    color = 0.7  # 색 부분의 비율 (0.0 ~ 1.0 사이의 값)
+    color_width = int(color * 100)  # 색 부분의 너비 계산
     if True:
         st.title(f"위험도는 \' {danger[0]} \' 입니다.")
-    
-        color = 0.7  # 색 부분의 비율 (0.0 ~ 1.0 사이의 값)
-        color_width = int(color * 100)  # 색 부분의 너비 계산
         color_bar_style = f'background-color: #DD5E65; height: 100%; width: {color_width}%;'
     
     elif False:
         st.title(f"위험도는 \' {danger[1]} \' 입니다.")
-    
-        color = 0.7  # 색 부분의 비율 (0.0 ~ 1.0 사이의 값)
-        color_width = int(color * 100)  # 색 부분의 너비 계산
         color_bar_style = f'background-color: #F0BD6A; height: 100%; width: {color_width}%;'
     
     elif False:
         st.title(f"위험도는 \' {danger[2]} \' 입니다.")
-    
-        color = 0.7  # 색 부분의 비율 (0.0 ~ 1.0 사이의 값)
-        color_width = int(color * 100)  # 색 부분의 너비 계산
         color_bar_style = f'background-color: #89BF6C; height: 100%; width: {color_width}%;'
     
     bar = f'<div style="{bar_style}"><div style="{color_bar_style}"></div></div>'
     new_color = int(color*100)
-    st.write("상세 위험도는 \' {color} % \' 입니다.")
+    st.write("상세 위험도는 \' {new_color} % \' 입니다.")
     st.markdown(bar, unsafe_allow_html=True)
     
     st.write(df)
