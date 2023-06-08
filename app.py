73b4badc152eb9015d6c0db1d5b76cc29bba488c
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-08 19:19:05 +0900

    sd

--- app.py
+++ app.py
@@ -47,16 +47,7 @@ with col1 :
     facility = ['건축', '산업환경설비', '조경', '토목', '기타']
     selected_facility = st.selectbox('시설물종류를 선택해주세요',facility)
 string = '시설물 종류_'
-selected_facility = '건축'
 df.loc[string + selected_facility] = 1.0
-if (selected_facility == '건축'): 
-    df.loc['시설물 종류_건축'] = 1.0
-elif (selected_facility == '산업환경설비'):
-    df.loc['시설물 종류_산업환경설비'] = 1.0
-elif (selected_facility == '조경'):
-    df.loc['시설물 종류_조경'] = 1.0
-else: 
-    df.loc['시설물 종류_토목'] = 1.0
     
 #(4) 공정률 (~%)
 with col2 :
@@ -64,9 +55,12 @@ with col2 :
 if percent:
     df.loc['공정률'] = float(percent)
 else:
-    df.loc['공정률'] = None
+    df.loc['공정률'] = None #이건 왜 있는거야? <- 이거 if문으로 안하니까 ㅇ
+    # #근데 그 구글폼에서 다음섹션으로 넘어가는 것처럼도 괜찮으면 if문 없어도 되고 음모르겟다람쥐ㅣ나돜ㅋㅋㅋㅋ 다람ㅣ 일단 보류옥퀴
+    # st.error("공사기간 입력 오류입니다. 입력한 공사 시작일과 종료일을 다시 한 번 확인해주세요.")
+    
 
-#(6),(5) 공종 (철근콘크리트~)
+    #(6),(5) 공종 (철근콘크리트~)
 with col2:
     category = ['가설공사', '강구조물공사', '건축 토공사', '건축물 부대공사', '관공사', '관공사 부대공사', '교량공사', '금속공사', '기계설비공사', '댐 및 제방공사', '도로 및 포장공사', '도장공사', '말뚝공사', '목공사', '미장공사', '방수공사', '산업설비공사', '수장공사', '전기설비공사', '조경공사', '조적공사', '지반개량공사', '지반조사', '지붕 및 홈통공사', '지정공사', '창호 및 유리공사', '철골공사', '철근콘크리트공사', '철도 및 궤도공사', '타일 및 돌공사', '터널공사', '토공사', '통신설비공사', '특수 건축물공사', '프리캐스트 콘크리트공사', '하천공사', '항만공사', '해체 및 철거공사', '기타']
     search_term = st.text_input('공종 검색어 입력을 통해 빠르게 검색할 수 있습니다', value="", placeholder="금속공사라면 '금속'을 검색해보세요")
@@ -74,13 +68,15 @@ with col2:
     filtered_category = [item for item in category if search_term.lower() in item.lower()]
 with col1:
     selected_category = st.selectbox('공종을 선택해주세요', filtered_category)
-
+string = '공종_'
+df.loc[string + selected_category] = 1.0
 
 #(7) 설계안전성검토 (대상,비대상)
 with col1 :
     selected_item2 = st.radio("설계안전성검토를 선택해주세요", ("대상", "비대상"))
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
+if (selected_item2 == "대상"):
+    df['설계안전성검토']
 #(8) 시설관리공사 (공공,민간)
 with col2 :
     selected_item3 = st.radio("시설관리공사를 선택해주세요", ("공공", "민간"))
@@ -103,8 +99,8 @@ st.write(' ')
 button_clicked = st.button('위험도 예측 결과 확인')
 danger = ["하", "중", "상"]
 if button_clicked:
-    #if 조건식1:
-        #st.write(f"위험도는",{danger[0]},\"입니다.")
+    if True:
+        st.write(f"위험도는",{danger[0]},\"입니다.")
     #elif 조건식2:
         #st.write(f"위험도는",{danger[1]},\"입니다.")
     #elif 조건식3:
