4c516b00e34547cd489c38de4cc59ddd9422d36a
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-12 16:11:40 +0900

    a

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if button_clicked:
     elif True:
         st.title(f"위험도는 '{danger[0]}' 입니다.")
         color_width = int(color * 25)  # 색 부분의 너비 계산
-        green_bar_style = f'background-color: #89BF6C; height: 8px; width: 25%; display: inline-block;'
+        green_bar_style = f'background-color: #F0BD6A; height: 8px; width: 25%; display: inline-block;'
         color_bar_style = f'background-color: #F0BD6A; height: 8px; width: {color_width}%; display: inline-block;'
         yellow_bar_style = f'background-color: #FDEDD0; height: 8px; width: {25-color_width}%; display: inline-block;'
         red_bar_style = f'background-color: #F2D0CD; height: 8px; width: 50%; display: inline-block;'
@@ -37,8 +37,16 @@ if button_clicked:
             unsafe_allow_html=True)
     
     elif False:
-        st.title(f"위험도는 \' {danger[2]} \' 입니다.")
-        color_bar_style = f'background-color: #DD5E65; height: 100%; width: {color_width}%;'
+        st.title(f"위험도는 '{danger[0]}' 입니다.")
+        color_width = int(color * 25)  # 색 부분의 너비 계산
+        green_bar_style = f'background-color: #89BF6C; height: 8px; width: 25%; display: inline-block;'
+        color_bar_style = f'background-color: #F0BD6A; height: 8px; width: {color_width}%; display: inline-block;'
+        yellow_bar_style = f'background-color: #FDEDD0; height: 8px; width: {25-color_width}%; display: inline-block;'
+        red_bar_style = f'background-color: #F2D0CD; height: 8px; width: 50%; display: inline-block;'
+        st.write(f"상세 위험도는 \' {int(color*100)} % \' 입니다.")
+        st.markdown(
+            f'<div style="{green_bar_style}"></div><div style="{color_bar_style}"></div><div style="{yellow_bar_style}"></div><div style="{red_bar_style}"></div>',
+            unsafe_allow_html=True)
     
 
 
