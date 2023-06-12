86029a7b51a9c2d336c7a0982d541a60b970d9c3
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-12 16:26:55 +0900

    a

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ if button_clicked:
     color = 0.7  # 색 부분의 비율 (0.0 ~ 1.0 사이의 값)
     if True:
         st.title(f"위험도는 '{danger[0]}' 입니다.")
-        color_width = int(color * 100)  # 색 부분의 너비 계산
-        color_bar_style = f'background-color: #89BF6C; height: 8px; width: {color_width-5}%; display: inline-block;'
-        green_bar_style = f'background-color: #E4F4CF; height: 8px; width: {100-color_width-5}%; display: inline-block;'
+        color_width = int(color * 95)  # 색 부분의 너비 계산
+        color_bar_style = f'background-color: #89BF6C; height: 8px; width: {color_width}%; display: inline-block;'
+        green_bar_style = f'background-color: #E4F4CF; height: 8px; width: {100-color_width}%; display: inline-block;'
         yellow_bar_style = f'background-color: #FDEDD0; height: 8px; width: 5%; display: inline-block;'
         st.write(f"상세 위험도는 \' {int(color*100)} % \' 입니다.")
         st.markdown(
