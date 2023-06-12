8f65e026c3da9f542d38e99c46e4ce8339d86473
yawn11 <bberry1106@hanyang.ac.kr>
2023-06-12 15:57:06 +0900

    a

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ danger = ["하", "중", "상"]
 if button_clicked:
     bar_style = 'background-color: #F0F2F6; height: 8px; width: 100%;'
     color = 0.7  # 색 부분의 비율 (0.0 ~ 1.0 사이의 값)
-    color_width = int(color * 25)  # 색 부분의 너비 계산
+    color_width = int(color * 100)  # 색 부분의 너비 계산
     if True:
         st.title(f"위험도는 \' {danger[0]} \' 입니다.")
         color_bar_style = f'background-color: #89BF6C; height: 100%; width: {color_width}%;'
